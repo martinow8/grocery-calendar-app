a8b86467fe89d748109820466342619c83b65048
Ivan Martinov <55989740+martinow8@users.noreply.github.com>
2026-09-21 10:14:30 +0300

    Update service worker to focus on grocery calendar app

--- sw.js
+++ sw.js
@@ -36,9 +36,9 @@ self.addEventListener('notificationclick', (event) => {
   event.waitUntil(
     clients.matchAll({ type: 'window' }).then((clientList) => {
       for (const client of clientList) {
-        if (client.url === '/' && 'focus' in client) return client.focus();
+        if (client.url.includes('/grocery-calendar-app/') && 'focus' in client) return client.focus();
       }
-      if (clients.openWindow) return clients.openWindow('/');
+      if (clients.openWindow) return clients.openWindow('/grocery-calendar-app/');
     })
   );
 });
