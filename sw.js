fdf8289cb65c0a6a5c67a22d9b0ee2a541b701dc
Ivan Martinov <55989740+martinow8@users.noreply.github.com>
2026-09-21 10:10:25 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,36 @@
+const CACHE_NAME = 'famsync-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  'https://cdn-icons-png.flaticon.com/512/3900/3900235.png'
+];
+
 self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(clients.claim());
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((key) => key !== CACHE_NAME && caches.delete(key)))
+    )
+  );
+  self.clients.claim();
 });
 
-// При кликване върху известието на телефона
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
+
+// Клик върху известието
 self.addEventListener('notificationclick', (event) => {
   event.notification.close();
   event.waitUntil(
